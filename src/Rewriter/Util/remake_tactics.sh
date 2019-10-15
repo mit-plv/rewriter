ac304ec5cf95d4ab96c398251a1a42cb6ecc030a
Jason Gross <jgross@mit.edu>
2019-10-15 15:44:00 -0400

    Remove some files

--- src/Rewriter/Util/remake_tactics.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cat > Tactics.v <<EOF
-(** * Generic Tactics *)
-Require Export Crypto.Util.FixCoqMistakes.
-EOF
-FILES="$(cd Tactics; git ls-files '*.v')"
-for i in $FILES; do
-    echo "Require Export Crypto.Util.Tactics.${i%.v}." >> Tactics.v
-done
