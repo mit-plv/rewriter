2ba3a18301f74192ded16a13ed576c79242efd13
Jason Gross <jgross@mit.edu>
2022-09-06 11:26:05 +0530

    Port equation_to_parts to Ltac2
    <details><summary>Timing Diff</summary>
    <p>
    
    ```
       After |   Peak Mem | File Name                                                     |   Before |   Peak Mem ||    Change || Change (mem) | % Change | % Change (mem)
    ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
    4m02.40s | 1382528 ko | Total Time / Peak Mem                                         | 4m01.88s | 1375752 ko || +0m00.51s ||      6776 ko |   +0.21% |         +0.49%
    ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
    0m54.11s | 1113204 ko | Rewriter/Rewriter/Examples/PerfTesting/LiftLetsMap.vo         | 0m54.18s | 1106992 ko || -0m00.07s ||      6212 ko |   -0.12% |         +0.56%
    0m54.03s | 1382528 ko | Rewriter/Rewriter/Examples/PerfTesting/SieveOfEratosthenes.vo | 0m53.85s | 1375752 ko || +0m00.17s ||      6776 ko |   +0.33% |         +0.49%
    0m51.78s | 1033788 ko | Rewriter/Rewriter/Examples.vo                                 | 0m51.57s | 1033128 ko || +0m00.21s ||       660 ko |   +0.40% |         +0.06%
    0m27.88s |  909444 ko | Rewriter/Rewriter/Examples/PrefixSums.vo                      | 0m27.77s |  908936 ko || +0m00.10s ||       508 ko |   +0.39% |         +0.05%
    0m23.56s |  881316 ko | Rewriter/Rewriter/Examples/PerfTesting/Plus0Tree.vo           | 0m23.53s |  875576 ko || +0m00.02s ||      5740 ko |   +0.12% |         +0.65%
    0m15.59s |  737684 ko | Rewriter/Rewriter/Examples/PerfTesting/UnderLetsPlus0.vo      | 0m15.57s |  729844 ko || +0m00.01s ||      7840 ko |   +0.12% |         +1.07%
    0m12.03s |  636536 ko | Rewriter/Demo.vo                                              | 0m12.03s |  636356 ko || +0m00.00s ||       180 ko |   +0.00% |         +0.02%
    0m00.81s |  471092 ko | Rewriter/Rewriter/Reify.vo                                    | 0m00.73s |  470928 ko || +0m00.08s ||       164 ko |  +10.95% |         +0.03%
    0m00.68s |  488236 ko | Rewriter/Rewriter/AllTactics.vo                               | 0m00.74s |  488220 ko || -0m00.05s ||        16 ko |   -8.10% |         +0.00%
    0m00.51s |  478536 ko | Rewriter/Util/plugins/RewriterBuildRegistryImports.vo         | 0m00.46s |  478620 ko || +0m00.04s ||       -84 ko |  +10.86% |         -0.01%
    0m00.50s |  480236 ko | Rewriter/Util/plugins/RewriterBuild.vo                        | 0m00.48s |  480064 ko || +0m00.02s ||       172 ko |   +4.16% |         +0.03%
    0m00.47s |  480280 ko | Rewriter/Util/plugins/RewriterBuildRegistry.vo                | 0m00.47s |  480124 ko || +0m00.00s ||       156 ko |   +0.00% |         +0.03%
    0m00.46s |  482136 ko | Rewriter/Rewriter/Examples/PerfTesting/Settings.vo            | 0m00.51s |  481932 ko || -0m00.04s ||       204 ko |   -9.80% |         +0.04%
    
    ```
    </p>
    </details>

--- src/Rewriter/Rewriter/Reify.v
+++ src/Rewriter/Rewriter/Reify.v
@@ -264,39 +264,54 @@ Module Compilers.
                            refine_reify_under_forall_types (Ltac1.get_to_constr "base_type" base_type) (Ltac1.get_to_constr "base_type_interp" base_type_interp) (Fresh.Free.of_goal ()) (Ltac1.get_to_constr "lem" lem) cont) in
         constr:(ltac:(f base_type base_type_interp lem ltac:(fun ty_ctx cur_i lem => let v := cont ty_ctx cur_i lem in refine v))).
 
-      Ltac prop_to_bool H := eval cbv [decb] in (decb H).
+      (* uses typeclass resolution *)
+      Ltac2 prop_to_bool (h : constr) : constr := eval cbv [decb] in constr:(decb $h).
 
+      Ltac2 push_side_conditions (h : constr) (side_conditions : constr) : constr :=
+        Reify.Constr.debug_check_strict "push_side_conditions" (fun () => mkApp '@cons ['bool; h; side_conditions]).
 
-      Ltac push_side_conditions H side_conditions :=
-        constr:(H :: side_conditions).
-
-      Ltac equation_to_parts' lem side_conditions :=
-        lazymatch lem with
-        | ?H -> ?P
-          => let __ := lazymatch type of H with
-                       | Prop => constr:(I)
-                       | ?T => constr_fail_with ltac:(fun _ => fail 1 "Invalid non-Prop non-dependent hypothesis of type" H ":" T "when reifying a lemma of type" lem)
-                       end in
-             let H := prop_to_bool H in
-             let side_conditions := push_side_conditions H side_conditions in
-             equation_to_parts' P side_conditions
-        | forall x : ?T, ?P
-          => let P' := fresh in
-             constr:(
-               fun x : T
-               => match P return _ with
-                  | P'
-                    => ltac:(let P := (eval cbv delta [P'] in P') in
-                             clear P';
-                             let res := equation_to_parts' P side_conditions in
-                             exact res)
-                  end)
-        | @eq ?T ?A ?B
-          => constr:((@eq T A B, side_conditions))
-        | ?T => constr_fail_with ltac:(fun _ => fail 1 "Invalid type of equation:" T)
-        end.
+      Ltac2 Type exn ::= [ Reification_missing_reflect_instance (constr, exn) ].
+      Ltac2 rec equation_to_parts' (lem : constr) (side_conditions : constr) : constr :=
+        Reify.debug_wrap
+          "equation_to_parts'" (fun (lem, side_conditions) => fprintf "%t (side: %t)" lem side_conditions) (lem, side_conditions)
+          Reify.should_debug_fine_grained Reify.should_debug_fine_grained (Some Message.of_constr)
+          (fun ()
+           => let debug_Constr_check := Reify.Constr.debug_check_strict "equation_to_parts'" in
+              lazy_match! lem with
+              | ?h -> ?p
+                => let t := Constr.type h in
+                   (if Constr.equal t 'Prop
+                    then ()
+                    else Control.zero (Reification_failure (fprintf "Invalid non-Prop non-dependent hypothesis of type %t : %t when reifying a lemma of type %t" h t lem)));
+                   let h := match Control.case (fun () => prop_to_bool h) with
+                            | Val h => let (h, _) := h in h
+                            | Err err => Control.zero (Reification_failure (fprintf "Missing Bool.reflect instance for %t: %a" lem (fun () => Message.of_exn) err))
+                            end in
+                   let side_conditions := push_side_conditions h side_conditions in
+                   equation_to_parts' p side_conditions
+              | @eq ?t ?a ?b
+                => '((@eq $t $a $b, $side_conditions))
+              | ?t
+                => match Constr.Unsafe.kind t with
+                   | Constr.Unsafe.Cast t _ _ => equation_to_parts' t side_conditions
+                   | Constr.Unsafe.Prod b p
+                     => (* we use in_context so we can do typeclass resolution later *)
+                       Constr.in_fresh_context_avoiding
+                         @x true (Some (Fresh.Free.of_constr lem)) [b]
+                         (fun ns
+                          => let p := debug_Constr_check (fun () => Constr.Unsafe.substnl (List.map (fun (n, _) => mkVar n) ns) 0 p) in
+                             let res := equation_to_parts' p side_conditions in
+                             Control.refine (fun () => res))
+                   | _ => Control.zero (Reification_failure (fprintf "Invalid type of equation: %t" t))
+                   end
+              end).
+      Ltac2 equation_to_parts (lem : constr) : constr :=
+        equation_to_parts' lem '(@nil bool).
+      #[deprecated(since="8.15",note="Use Ltac2 instead.")]
       Ltac equation_to_parts lem :=
-        equation_to_parts' lem (@nil bool).
+        let f := ltac2:(lem
+                        |- Control.refine (fun () => equation_to_parts (Ltac1.get_to_constr "lem" lem))) in
+        constr:(ltac:(f lem)).
 
       Ltac preadjust_pattern_type_variables pat :=
         let pat := (eval cbv [pattern.type.relax pattern.type.subst_default pattern.type.subst_default_relax pattern.type.unsubst_default_relax] in pat) in
