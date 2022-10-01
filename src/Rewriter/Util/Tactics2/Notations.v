43346093311b54236f28a3d0c5431c8819bbb16c
Jason Gross <jgross@mit.edu>
2022-10-01 10:39:15 +0530

    Add occurrences:(...) Ltac2 notation

--- src/Rewriter/Util/Tactics2/Notations.v
+++ src/Rewriter/Util/Tactics2/Notations.v
@@ -2,3 +2,4 @@ Require Import Ltac2.Ltac2.
 Require Export Rewriter.Util.FixCoqMistakes.
 
 Ltac2 Notation "strategy:(" s(strategy) ")" := s.
+Ltac2 Notation "occurrences:(" o(occurrences) ")" := o.
