ec45186661a67d815064cf83d0540107c65b3844
Jason Gross <jgross@mit.edu>
2022-10-06 21:33:18 +0530

    Generalize in_evar to allow return values

--- src/Rewriter/Util/Tactics2/InstantiateEvar.v
+++ src/Rewriter/Util/Tactics2/InstantiateEvar.v
@@ -1,10 +1,12 @@
 Require Import Ltac2.Ltac2.
 Require Export Rewriter.Util.FixCoqMistakes.
 
-Ltac2 in_evar (e : evar) (t : unit -> unit) : unit :=
+Ltac2 in_evar (e : evar) (t : unit -> 'a) : 'a :=
+  let cell : 'a option ref := { contents := None } in
   Control.new_goal e >
     [ ..
-    | t (); Control.shelve () ].
+    | let v := t () in cell.(contents) := Some v; Control.shelve () ];
+  Option.get (cell.(contents)).
 
 Module Evar.
   Ltac2 instantiate (e : evar) (c : unit -> constr) : unit :=
